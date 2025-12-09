a91a18508fa4ce2c4b64bed3514ef2b2f1df4e1c
Kevin Harrington <mad.hephaestus@gmail.com>
2025-12-09 13:03:20 -0500

    Create loadGit.groovy

---
+++ loadGit.groovy
@@ -0,0 +1,16 @@
+import com.neuronrobotics.bowlerstudio.BowlerKernel;
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
+import com.neuronrobotics.bowlerstudio.assets.AssetFactory;
+
+
+BowlerKernel.ensureUpdated(true,"https://github.com/CommonWealthRobotics/ExternalEditorsBowlerStudio.git",
+    "https://github.com/CommonWealthRobotics/freecad-bowler-cli.git",
+    "https://github.com/CommonWealthRobotics/blender-bowler-cli.git",
+    "https://github.com/kennetek/gridfinity-rebuilt-openscad.git",
+    "https://github.com/CommonWealthRobotics/BowlerStudioExampleRobots.git",
+    AssetFactory.getGitSource(),
+    "https://github.com/madhephaestus/CaDoodle-Example-Objects.git",
+    "https://github.com/madhephaestus/carl-the-hexapod.git",
+    Vitamins.getGitRepoDatabase(), 
+    "https://github.com/CommonWealthRobotics/CaDoodle-ShapesPalet-Content.git");
+Vitamins.loadAllScriptFiles();
